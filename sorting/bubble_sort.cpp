8232c70c297b372c1bc8c5cb623301ea9d242bf9
aftaab25 <aftaab2507@gmail.com>
2024-01-13 21:20:22 +0530

    bubble sort code

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -9,6 +9,22 @@ void printArray(int arr[], int n) {
     cout << "\n";
 }
 
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n - i - 1; j++){
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+
+        if (swapped == false) {
+            break;
+        }
+
+    }
+}
 
 
 int main(){
