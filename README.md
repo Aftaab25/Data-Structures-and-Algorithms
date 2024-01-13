79b112e2fb33db78a5d9de5bcb115899c535fb90
aftaab25 <aftaab2507@gmail.com>
2024-01-13 20:56:16 +0530

    added files for sorting algorithms

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Data-Structures-and-Algorithms
 Data Structures and Algorithms in C++
+- Algorithms
---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    //
+}
+
+/*
+    6 0 3 6
+
+    i = 0 => j = 0 : 3
+    i = 1 => j = 0 : 2
+    i = 2 => j = 0 : 1
+    i = 3 => j = 0 : 0
+*/
