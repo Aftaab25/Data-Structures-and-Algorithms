761309669bc93af63952756010f8a4e9ff64b56b
aftaab25 <aftaab2507@gmail.com>
2024-01-13 21:01:26 +0530

    bubble sort gist

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -7,6 +7,8 @@ int main(){
 }
 
 /*
+    With each iteration, it tries to store the largest
+    element at the end of the array.
     6 0 3 6
 
     i = 0 => j = 0 : 3
@@ -14,3 +16,4 @@ int main(){
     i = 2 => j = 0 : 1
     i = 3 => j = 0 : 0
 */
+
