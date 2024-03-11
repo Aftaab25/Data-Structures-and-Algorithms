8ac273ca1e1fa22ebc144a3291b99722dd8bc1f0
aftaab25 <aftaab2507@gmail.com>
2024-03-11 19:01:37 +0530

    need to add logic for quick sort

---
+++ sorting/quick_sort.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void partition(vector<int>& arr, int low,int mid , int high) {
+
+}
+
+
+/*
+ * Quick Sort Logic:
+*/
+void quick_sort(vector<int>& arr, int low, int high) {
+}
+
+int main() {
+    vector<int> v = {4,2,1,6,7};
+    quick_sort(v, 0, v.size()-1);
+
+    for (int i: v) {
+        cout << i << ", ";
+    }
+    cout << endl;
+    return 0;
+}
