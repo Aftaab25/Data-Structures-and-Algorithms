b3d8465227960b0910e3f4063bb7da318cd67a35
aftaab25 <aftaab2507@gmail.com>
2024-03-11 19:23:19 +0530

    Added quick sort code

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -2,15 +2,43 @@
 
 using namespace std;
 
-void partition(vector<int>& arr, int low,int mid , int high) {
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
 
+    while (i<j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+    
+        while (arr[j] > pivot && j > low) {
+            j--;
+        }
+    
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[low], arr[j]);
+    return j;
 }
 
 
 /*
  * Quick Sort Logic:
+ * Pick a pivot index (low(0-th) index in this case)
+ * Move the pivot to its correct place in the sorted array
+ * Move the elements smaller than the pivot to the left of it
+ * Move the elements greater than the pivot to the right of it
 */
 void quick_sort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIdx = partition(arr, low, high);
+        quick_sort(arr, low, pivotIdx - 1);
+        quick_sort(arr, pivotIdx + 1, high);
+    }
 }
 
 int main() {
