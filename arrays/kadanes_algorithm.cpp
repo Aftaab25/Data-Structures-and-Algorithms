6c36c95fb3a9fcef8d5cfdc1b271bbdae88cfb7e
aftaab25 <aftaab2507@gmail.com>
2024-03-13 23:38:38 +0530

    kadane's algorithm

---
+++ arrays/kadanes_algorithm.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int find_max_subarray_sum(vector<int>& nums) {
+	// Kadane's ALgorithm
+
+	int sum = 0;
+	int maxSum = INT_MIN;
+	for (auto i: nums) {
+		sum += i;
+		if (sum > maxSum) {
+			maxSum = max(maxSum, sum);
+		}
+
+		if (sum < 0) sum = 0;
+
+	}
+
+	return maxSum;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    // For getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // For writing output to output.txt
+    freopen("output.txt", "w", stdout);
+#endif
+
+	vector<int> v;
+	int num;
+
+	while(cin >> num) {
+		v.emplace_back(num);
+	}
+
+	cout << find_max_subarray_sum(v) << endl;
+
+}
